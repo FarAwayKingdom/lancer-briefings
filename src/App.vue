f0571ede1380d763b7b0b34788da327ade95ed6e
FarAwayKingdom <134040633+FarAwayKingdom@users.noreply.github.com>
2023-09-06 20:05:32 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -125,12 +125,12 @@ export default {
           "mech": "Scarlet Judicator"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
+          "callsign": "COOP",
+          "alias": "Nick Braddoc",
+          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///DUCOTIER-OVERWRITE//6f5759-6946-41bf-931a-e05e892",
+          "corpro": "H0RUS",
+          "frame": "KOBOLD-PATTERN",
+          "mech": "Stayput"
         },
         {
           "callsign": "Alcatraz",
